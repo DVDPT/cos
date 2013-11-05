2b00ebbd319642370a5067b08b2fd2d7424868e6
Ricardo Nunes <ricardomiguel.sn@gmail.com>
2013-11-05 18:41:46 +0000

    Started Client Services Workflows

---
+++ code/web/BrandAnalytics/Common/GetClientActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using BrandAnalytics.Data;
+using BrandAnalytics.Database;
+
+namespace BrandAnalytics.Common
+{
+    public sealed class GetClientActivity : CodeActivity
+    {
+        public InArgument<int> ClientId { get; set; }
+
+        public OutArgument<Client> Client { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            context.SetValue(this.Client, BrandAnalyticsDatabaseFactory.Instance.Clients.Get(context.GetValue(this.ClientId)));
+        }
+    }
+}
