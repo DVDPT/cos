3cd6787e576927facf205e075a8575d4215503de
Ricardo Nunes <ricardomiguel.sn@gmail.com>
2013-11-15 13:37:39 +0000

    Client service with GetCompletedStudies request

---
+++ code/web/BrandAnalytics/BrandAnalytics.Service/ServiceActivities/ClientService/GetCompletedStudiesActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using BrandAnalytics.Data;
+using BrandAnalytics.Database;
+
+namespace BrandAnalytics.Service.ServiceActivities.ClientService
+{
+
+    public sealed class GetCompletedStudiesActivity : CodeActivity
+    {
+        public InArgument<int> ClientId { get; set; }
+
+        public OutArgument<TwitterStudyReport[]> Reports { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            context.SetValue(
+                Reports,
+                BrandAnalyticsDatabaseFactory.Instance.Studies.GetAll()
+                .Where(s => s.CurrentState == TwitterStudyStates.Completed && s.ClientId == context.GetValue(ClientId))
+                .Select(s => s.Report)
+                .ToArray()
+            );
+        }
+    }
+}
